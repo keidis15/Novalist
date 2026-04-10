b8e40e933052e425cc96a235d42c18c40805b151
keidisSuarez <keidissuarez16@gmail.com>
2026-04-10 17:27:46 -0400

    Feat: Agregando estructura client, server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./dataBase/db");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//ruta de prueba
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      message: "Conectado a Neon con exito",
+      time: result.row[0],
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor Novalist corriendo en puerto ${PORT}`);
+});
